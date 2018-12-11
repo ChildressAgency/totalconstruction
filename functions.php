ea98a804d00dc368d45e7e06e20fb13c0686e3dd
childressdev <jcampbell@childressagency.com>
2018-12-11 17:50:30 -0500

    added footer fallback menu

--- functions.php
+++ functions.php
@@ -95,3 +95,15 @@ function totalconstruction_header_fallback_menu(){ ?>
   </div>
 <?php
 }
+
+function totalconstruction_footer_fallback_menu(){ ?>
+  <nav id="footer-nav">
+    <ul class="nav nav-justified">
+      <li><a href="<?php echo home_url(); ?>">Home</a></li>
+      <li><a href="<?php echo home_url('about'); ?>">About</a></li>
+      <li><a href="<?php echo home_url('services'); ?>">Services</a></li>
+      <li><a href="<?php echo home_url('contact'); ?>">Contact</a></li>
+    </ul>
+  </nav>
+<?php
+}
