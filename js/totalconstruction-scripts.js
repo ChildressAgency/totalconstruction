00a65658b3eae8acecfda6185e728557e219f125
childressdev <jcampbell@childressagency.com>
2018-12-11 12:31:59 -0500

    gallery demo styled

--- js/totalconstruction-scripts.js
+++ js/totalconstruction-scripts.js
@@ -4,15 +4,60 @@ jQuery(document).ready(function($){
     slidesToScroll: 1,
     arrows: false,
     fade: true,
-    asNavFor: '.gallery'
+    asNavFor: '.gallery',
+    centerMode:true
   });
   $('.gallery').slick({
-    slidesToShow: 4,
+    slidesToShow: 6,
     slidesToScroll: 1,
     asNavFor: '.gallery-main',
     dots: false,
     centerMode:true,
-    focusOnSelect:true
+    focusOnSelect:true,
+    arrows:true,
+    prevArrow:'<button type="button" class="gallery-arrow slick-prev">Previous</button>',
+    nextArrow:'<button type="button" class="gallery-arrow slick-next">Next</button>',
+    responsive:[
+      {
+        breakpoint:991,
+        settings:{
+          slidesToShow:4,
+          slidesToScroll:1,
+          centerMode:true,
+          focusOnSelect:true,
+          arrows:true
+        }
+      },
+      {
+        breakpoint:767,
+        settings:{
+          slidesToShow:3,
+          slidesToScroll:1,
+          centerMode:true,
+          focusOnSelect:true,
+          arrows:true
+        }
+      },
+      {
+        breakpoint:570,
+        settings:{
+          slidesToShow:2,
+          slidesToScroll:1,
+          centerMode:true,
+          focusOnSelect:true,
+          arrows:true
+        }
+      },
+      {
+        breakpoint:400,
+        settings:{
+          slidesToShow:1,
+          slidesToScroll:1,
+          centerMode:true,
+          focusOnSelect:true
+        }
+      }
+    ]
   });
 
   var filterOn = false;
@@ -20,10 +65,11 @@ jQuery(document).ready(function($){
     var filter = '.' + $(this).find(':selected').val();
 
     if(filterOn === true){
-      $('.gallery-main, .gallery').slick('slickUnfilter');
+      $('.gallery').slick('slickUnfilter');
     }
 
-    $('.gallery-main, .gallery').slick('slickFilter', filter);
+    $('.gallery').slick('slickFilter', filter);
     filterOn = true;
+    //$(window).trigger('resize');
   });
 });
