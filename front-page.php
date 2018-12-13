65fc0cea464b9404e4db47e63d8c434bc2d7c94e
childressdev <jcampbell@childressagency.com>
2018-12-13 17:03:50 -0500

    moved service cards section to template part

--- front-page.php
+++ front-page.php
@@ -19,39 +19,7 @@
     <div class="bg-overlay"></div>
   </section>
 
-  <?php
-    $services = new WP_Query(array(
-      'post_type' => 'services',
-      'posts_per_page' => -1,
-      'post_status' => 'publish'
-    ));
-
-    if($services->have_posts()): ?>
-      <section id="hp-services">
-        <div class="container">
-          <div class="row">
-            <?php $s = 0; while($services->have_posts()): $services->the_post(); ?>
-              <?php if($s % 3 == 0){ echo '<div class="clearfix"></div>'; } ?>
-              <?php $service_id = get_the_ID(); ?>
-              <div class="col-sm-4">
-                <div class="service-card">
-                  <?php
-                    if(has_post_thumbnail()){
-                      the_post_thumbnail('full', array('class' => 'img-responsive center-block'));
-                    }
-                  ?>
-                  <div class="service-card-caption">
-                    <h2><?php the_title(); ?></h2>
-                    <?php the_excerpt(); ?>
-                  </div>
-                </div>
-              </div>
-            <?php endwhile; ?>
-          </div>
-          <a href="<?php echo home_url('services'); ?>" class="btn-main">Our Services</a>
-        </div>
-      </section>
-  <?php endif; wp_reset_postdata(); ?>
+  <?php get_template_part('includes/service-cards'); ?>
 
   <?php 
     $about_text = get_post_meta($page_id, 'about_section_text');
