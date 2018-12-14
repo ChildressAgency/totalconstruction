d4e7d6efdb53318c625c946b661160a64fa9f146
childressdev <jcampbell@childressagency.com>
2018-12-14 17:31:16 -0500

    added check for empty content

--- page-testimonials.php
+++ page-testimonials.php
@@ -2,13 +2,20 @@
   <?php $page_id = get_the_ID(); ?>
   <main id="main">
     <div class="container">
-      <?php if(have_posts()): ?>
-        <article>
-          <?php while(have_posts()): the_post(); ?>
-            <?php the_content(); ?>
-          <?php endwhile; ?>
-        </article>
-      <?php endif; ?>
+      <?php
+        if(have_posts()){
+          while(have_posts()){
+            the_post();
+            $content = get_the_content();
+            if($content){
+              echo '<article>' . wp_kses_post(apply_filters('the_content', $content)) . '</article>';
+            }
+          }
+        }
+        else{
+          echo '<article><p>Sorry, nothing was found.</p></article>';
+        }
+      ?>
 
       <?php
         $testimonials = get_post_meta($page_id, 'testimonials', true);
@@ -20,9 +27,10 @@
               </div>
               <div class="col-xs-11 col-sm-10<?php echo ($t % 2 == 0) ? '' : ' col-sm-pull-2'; ?>">
                 <article>
-                  <?php echo esc_html(get_post_meta($page_id, 'testimonials_' . $t . '_testimonial')); ?>
+                  <?php echo esc_html(get_post_meta($page_id, 'testimonials_' . $t . '_testimonial', true)); ?>
                   <footer>
-                    <p><?php echo esc_html(get_post_meta($page_id, 'testimonials_' . $t . '_testimonial_author')); ?></p>
+                    <?php $testimonial_author_title = get_post_meta($page_id, 'testimonials_' . $t . '_testimonial_author_title', true); ?>
+                    <p><?php echo esc_html(get_post_meta($page_id, 'testimonials_' . $t . '_testimonial_author', true)); ?><?php echo $testimonial_author_title ? '<span>' . esc_html($testimonial_author_title) . '</span>' : ''; ?></p>
                   </footer>
                 </article>
               </div>
