c91bbeb29b6de63f812a3b2dd30972e630688048
childressdev <jcampbell@childressagency.com>
2018-12-14 17:31:00 -0500

    fixed acf image call

--- page-about.php
+++ page-about.php
@@ -3,51 +3,63 @@
   <main id="main" class="about-us">
     <div class="container">
       <?php
-        $about_section_content = get_post_meta($page_id, 'about_section_content');
+        $about_section_content = get_post_meta($page_id, 'about_section_settings_content', true);
         if($about_section_content): ?>
           <article>
             <div class="row">
               <div class="col-sm-7">
-                <h2 class="block-font"><?php echo esc_html(get_post_meta($page_id, 'about_section_title')); ?></h2>
-                <?php echo esc_html($about_section_content); ?>
+                <h2 class="block-font"><?php echo esc_html(get_post_meta($page_id, 'about_section_settings_title', true)); ?></h2>
+                <?php echo wp_kses_post(apply_filters('the_content', $about_section_content)); ?>
               </div>
               <div class="col-sm-5">
-                <?php $about_section_image = get_post_meta($page_id, 'about_section_image', true); ?>
-                <img src="<?php echo $about_section_image['url']; ?>" class="img-responsive center-block" alt="<?php echo $about_section_image['title']; ?>" />
+                <?php 
+                  $about_section_image_id = get_post_meta($page_id, 'about_section_settings_image', true); 
+                  $about_section_image = wp_get_attachment_image_src($about_section_image_id, 'full');
+                  $about_section_image_alt = get_post_meta($about_section_image_id, '_wp_attachment_image_alt', true);
+                ?>
+                <img src="<?php echo esc_url($about_section_image[0]); ?>" class="img-responsive center-block" alt="<?php echo esc_html($about_section_image_alt); ?>" />
               </div>
             </div>
           </article>
       <?php endif; ?>
 
       <?php
-        $mission_section_content = get_post_meta($page_id, 'mission_section_content');
+        $mission_section_content = get_post_meta($page_id, 'mission_section_settings_content', true);
         if($mission_section_content): ?>
           <article>
             <div class="row">
               <div class="col-sm-7">
-                <h2><?php echo esc_html(get_post_meta($page_id, 'mission_section_title')); ?></h2>
-                <?php echo esc_html($mission_section_content); ?>
+                <h2><?php echo esc_html(get_post_meta($page_id, 'mission_section_settings_title', true)); ?></h2>
+                <?php echo wp_kses_post(apply_filters('the_content', $mission_section_content)); ?>
               </div>
               <div class="col-sm-5">
-                <?php $mission_section_image = get_post_meta($page_id, 'mission_section_image', true); ?>
-                <img src="<?php echo $mission_section_image['url']; ?>" class="img-responsive center-block" alt="<?php echo $mission_section_image['title']; ?>" />
+                <?php 
+                  $mission_section_image_id = get_post_meta($page_id, 'mission_section_settings_image', true); 
+                  $mission_section_image = wp_get_attachment_image_src($mission_section_image_id, 'full');
+                  $mission_section_image_alt = get_post_meta($mission_section_image_id, '_wp_attachment_image_alt', true);
+                ?>
+                <img src="<?php echo esc_url($mission_section_image[0]); ?>" class="img-responsive center-block" alt="<?php echo esc_attr($mission_section_image_alt); ?>" />
               </div>
             </div>
           </article>
       <?php endif; ?>
 
       <?php
-        $background_section_content = get_post_meta($page_id, 'background_section_content');
+        $background_section_content = get_post_meta($page_id, 'background_section_settings_content', true);
         if($background_section_content): ?>
           <article>
             <div class="row">
               <div class="col-sm-7">
-                <h2><?php echo esc_html(get_post_meta($page_id, 'background_section_title')); ?></h2>
-                <?php echo esc_html($background_section_content); ?>
+                <h2><?php echo esc_html(get_post_meta($page_id, 'background_section_settings_title', true)); ?></h2>
+                <?php echo wp_kses_post(apply_filters('the_content', $background_section_content)); ?>
               </div>
               <div class="col-sm-5">
-                <?php $background_section_image = get_post_meta($page_id, 'background_section_image'); ?>
-                <img src="<?php echo $background_section_image ? $background_section_image['url'] : 'https://via.placeholder.com/410x500/e6e6e6?text=Photo+of+Keith+coming+later'; ?>" class="img-responsive center-block" alt="<?php echo $background_section_image['title']; ?>" />
+                <?php 
+                  $background_section_image_id = get_post_meta($page_id, 'background_section_settings_image', true); 
+                  $background_section_image = wp_get_attachment_image_src($background_section_image_id, 'full');
+                  $background_section_image_alt = get_post_meta($background_section_image_id, '_wp_attachment_image_alt', true);
+                ?>
+                <img src="<?php echo $background_section_image ? esc_url($background_section_image[0]) : 'https://via.placeholder.com/410x500/e6e6e6?text=Photo+of+Keith+coming+later'; ?>" class="img-responsive center-block" alt="<?php echo esc_attr($background_section_image_alt); ?>" />
               </div>
             </div>
           </article>
@@ -58,8 +70,8 @@
   <?php get_template_part('includes/service-cards'); ?>
 
   <?php
-    $awards = get_post_meta($page_id, 'awards');
-    $partners = get_post_meta($page_id, 'partners');
+    $awards = get_post_meta($page_id, 'awards', true);
+    $partners = get_post_meta($page_id, 'partners', true);
 
     if($awards || $partners): ?>
       <section id="awards-partners">
@@ -71,9 +83,13 @@
               <ul class="list-unstyled list-inline">
                 <?php for($a = 0; $a < $awards; $a++): ?>
                   <li>
-                    <?php $awards_image = get_post_meta($page_id, 'awards_' . $a . '_award_image', true); ?>
-                    <img src="<?php echo $awards_image['url']; ?>" class="img-responsive center-block" alt="<?php echo $awards_image['title']; ?>" />
-                    <p><?php echo esc_html(get_post_meta($page_id, 'awards_' . $a . '_award_description')); ?></p>
+                    <?php 
+                      $awards_image_id = get_post_meta($page_id, 'awards_' . $a . '_award_image', true); 
+                      $awards_image = wp_get_attachment_image_src($awards_image_id, 'full');
+                      $awards_image_alt = get_post_meta($awards_image_id, '_wp_attachment_image_alt', true);
+                    ?>
+                    <img src="<?php echo esc_url($awards_image[0]); ?>" class="img-responsive center-block" alt="<?php echo esc_attr($awards_image_alt); ?>" />
+                    <p><?php echo esc_html(get_post_meta($page_id, 'awards_' . $a . '_award_description', true)); ?></p>
                   </li>
                 <?php endfor; ?>
               </ul>
@@ -86,9 +102,13 @@
               <ul class="list-unstyled list-inline">
                 <?php for($p = 0; $p < $partners; $p++): ?>
                   <li>
-                    <a href="<?php echo esc_url(get_post_meta($page_id, 'partners_' . $p . '_partner_link')); ?>" target="_blank">
-                      <?php $partner_image = get_post_meta($page_id, 'partners_' . $p . '_partner_image', true); ?>
-                      <img src="<?php echo $partner_image['url']; ?>" class="img-responsive center-block" alt="<?php echo $partner_image['title']; ?>" />
+                    <a href="<?php echo esc_url(get_post_meta($page_id, 'partners_' . $p . '_partner_link', true)); ?>" target="_blank">
+                      <?php 
+                        $partner_image_id = get_post_meta($page_id, 'partners_' . $p . '_partner_image', true); 
+                        $partner_image = wp_get_attachment_image_src($partner_image_id, 'full');
+                        $partner_image_alt = get_post_meta($partner_image_id, '_wp_attachment_image_alt', true);
+                      ?>
+                      <img src="<?php echo esc_url($partner_image[0]); ?>" class="img-responsive center-block" alt="<?php echo esc_attr($partner_image); ?>" />
                     </a>
                   </li>
                 <?php endfor; ?>
