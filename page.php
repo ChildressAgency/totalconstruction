eaf0af3d0f9337e07b27eac4e49884a662db8a35
childressdev <jcampbell@childressagency.com>
2018-12-14 17:30:40 -0500

    added check for empty content

--- page.php
+++ page.php
@@ -1,13 +1,20 @@
 <?php get_header(); ?>
 <main id="main">
   <div class="container">
-    <article>
-      <?php if(have_posts()): while(have_posts()): the_post(); ?>
-        <?php the_content(); ?>
-      <?php endwhile; else: ?>
-        <p>Sorry, nothing was found.</p>
-      <?php endif; ?>
-    </article>
+    <?php
+      if(have_posts()){
+        while(have_posts()){
+          the_post();
+          $content = get_the_content();
+          if($content){
+            echo '<article>' . wp_kses_post(apply_filters('the_content', $content)) . '</article>';
+          }
+        }
+      }
+      else{
+        echo '<article><p>Sorry, nothing was found.</p></article>';
+      }
+    ?>
   </div>
 </main>
 <?php get_footer(); ?>
