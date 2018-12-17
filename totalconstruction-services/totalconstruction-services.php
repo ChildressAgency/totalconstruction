3efb7b52560076aa973295f29fdc9bcbe1e289ce
childressdev <jcampbell@childressagency.com>
2018-12-17 13:30:05 -0500

    added gallery acf

--- totalconstruction-services/totalconstruction-services.php
+++ totalconstruction-services/totalconstruction-services.php
@@ -154,4 +154,52 @@ function totalconstruction_add_services_acf_fields(){
     'description' => '',
   ));
 
+  acf_add_local_field_group(array(
+    'key' => 'group_5c17ba38e2ef2',
+    'title' => 'Services Gallery Settings',
+    'fields' => array(
+      array(
+        'key' => 'field_5c17ba7025d15',
+        'label' => 'Gallery',
+        'name' => 'gallery',
+        'type' => 'gallery',
+        'instructions' => '',
+        'required' => 0,
+        'conditional_logic' => 0,
+        'wrapper' => array(
+          'width' => '',
+          'class' => '',
+          'id' => '',
+        ),
+        'min' => '',
+        'max' => '',
+        'insert' => 'append',
+        'library' => 'all',
+        'min_width' => '',
+        'min_height' => '',
+        'min_size' => '',
+        'max_width' => '',
+        'max_height' => '',
+        'max_size' => '',
+        'mime_types' => '',
+      ),
+    ),
+    'location' => array(
+      array(
+        array(
+          'param' => 'post_type',
+          'operator' => '==',
+          'value' => 'services',
+        ),
+      ),
+    ),
+    'menu_order' => 0,
+    'position' => 'normal',
+    'style' => 'default',
+    'label_placement' => 'top',
+    'instruction_placement' => 'label',
+    'hide_on_screen' => '',
+    'active' => 1,
+    'description' => '',
+  ));
 }
